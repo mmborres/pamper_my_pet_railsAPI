688db9d3fc0055ad6b1e8805480f049095d895c1
Mabeth <mmborres@gmail.com>
2019-05-11 10:19:06 +1000

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,6 @@ module PetShopServer
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.logger = Logger.new(STDOUT)
   end
 end
